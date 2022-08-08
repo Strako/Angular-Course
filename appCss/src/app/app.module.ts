f47b2d6c8a414329de01a3aa3c25c05112e13f59
Armando Ibarra <70788529+Strako@users.noreply.github.com>
2022-08-08 08:13:27 -0500

    testing

--- appCss/src/app/app.module.ts
+++ appCss/src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './components/app/app.component';
+import { TestComponent } from './components/test/test.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule
