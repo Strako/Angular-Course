143c626f35820d341f1e69974ab9f8cd1b127fee
Armando Ibarra <70788529+Strako@users.noreply.github.com>
2022-08-12 11:29:39 -0500

    added light/dark switch button
    light and dak mode can be witched by a flotating button

--- cursoIICss/src/app/app.component.ts
+++ cursoIICss/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { DOCUMENT } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cursoIICss';
+  mode = 1;
+  arrMdTex = ['Light','Dark'];
+  switch(){
+    if (this.mode === 1){
+      this.mode = 0
+      console.log(this.mode)
+      
+    }else{
+      this.mode = 1
+            console.log(this.mode)
+    }
+  }
 }
